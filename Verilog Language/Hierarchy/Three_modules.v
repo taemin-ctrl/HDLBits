e66777833b84d109662b52cb4d635f07023dafce
taemin-ctrl <ktm0106@naver.com>
2025-02-18 22:53:50 +0900

    Create Three_modules.v

---
+++ Verilog Language/Hierarchy/Three_modules.v
@@ -0,0 +1,8 @@
+module top_module ( input clk, input d, output q );
+    // wires
+    wire q1, q2;
+    // instances
+    my_dff inst1(.clk(clk), .d(d), .q(q1));
+    my_dff inst2(.clk(clk), .d(q1), .q(q2));
+    my_dff inst3(.clk(clk), .d(q2), .q(q));
+endmodule
