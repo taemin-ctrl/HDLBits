8aa15a1d4b641fbcfbf1d16824e694f090c1aefd
taemin-ctrl <ktm0106@naver.com>
2025-02-20 13:54:23 +0900

    Create 256-to-1_4-bit_multiplexer.v

---
+++ Circuits/Combinational Logic/Multiplexers/256-to-1_4-bit_multiplexer.v
@@ -0,0 +1,10 @@
+module top_module( 
+    input [1023:0] in,
+    input [7:0] sel,
+    output [3:0] out );
+    
+    assign out[0] = in[4*sel];
+    assign out[1] = in[4*sel+1];
+    assign out[2] = in[4*sel+2];
+    assign out[3] = in[4*sel+3];
+endmodule
