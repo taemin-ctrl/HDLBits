38e2b8ccc46d0d7ac7cae59ad2a5cf6276dc903f
taemin-ctrl <ktm0106@naver.com>
2025-02-27 10:04:06 +0900

    Create Dual-edge_triggered_flip-flop.v

---
+++ Circuits/Squential Logic/Latches and Flip-Flops/Dual-edge_triggered_flip-flop.v
@@ -0,0 +1,16 @@
+module top_module (
+    input clk,
+    input d,
+    output q
+);
+    reg q1, q2;
+    wire m;
+    always @(posedge clk) begin
+		q1 <= d;
+    end
+    
+    always @(negedge clk) begin
+    	q2 <= d;
+    end
+	assign q = clk ? q1 : q2;
+endmodule
